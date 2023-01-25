ca474ac0b45efd1dc2ed833c3897afb23be0d2ea
SrushtiPatil1 <83879917+SrushtiPatil1@users.noreply.github.com>
2023-01-25 10:39:25 +0530

    Create javafile.java

---
+++ javafile.java
@@ -0,0 +1,9 @@
+import java.util.*;  
+public class javafile 
+{  
+public static void main(String[] args)  
+{  
+String str= "Srushti";                
+System.out.print("String Length: "+str.length());            
+}  
+}  
